a3eea076ea44ae3ac6d041ba4b67da36501da209
Andrew McOlash <amcolash@gmail.com>
2016-05-01 18:19:16 -0500

    Add in support for GifMe and Riffsy

--- GiffingAwesome/www/js/controllers.js
+++ GiffingAwesome/www/js/controllers.js
@@ -4,17 +4,20 @@ angular.module('starter.controllers', [])
 })
 
 .controller('SearchController', function($scope, $http, $ionicModal) {
-  $scope.images = [];
-  $scope.search = 'cats';
+  $scope.api = 'Giphy';
   $scope.hq = false;
+  $scope.search = 'cats';
+
+  $scope.images = [];
   $scope.offset = 0;
   $scope.limit = 25;
   $scope.lastData = [];
 
   $scope.loadImages = function() {
-    $http.get('http://api.giphy.com/v1/gifs/search?q=' + $scope.search + '&limit=' + $scope.limit +
-      '&offset=' + $scope.offset + '&api_key=dc6zaTOxFJmzC')
-      .then(function(response) {
+    if ($scope.api === 'Giphy') {
+      $http.get('http://api.giphy.com/v1/gifs/search?q=' + $scope.search + '&limit=' + $scope.limit +
+        '&offset=' + $scope.offset + '&api_key=dc6zaTOxFJmzC')
+        .then(function(response) {
           var data = response.data.data;
           $scope.lastData = data;
 
@@ -31,8 +34,51 @@ angular.module('starter.controllers', [])
 
           $scope.offset += $scope.limit;
           $scope.$broadcast('scroll.infiniteScrollComplete');
-        }
-      );
+        });
+    } else if ($scope.api === 'GifMe') {
+      $http.get('http://api.gifme.io/v1/search?query=' + $scope.search + '&limit=' + $scope.limit +
+        '&page=' + $scope.offset + '&key=rX7kbMzkGu7WJwvG')
+        .then(function(response) {
+          var data = response.data.data;
+          $scope.lastData = data;
+
+          console.log(data);
+          for(var i = 0; i < data.length; i++) {
+            $scope.images.push({
+              id: i,
+              imgUrl: data[i].link,
+              hqImgUrl: data[i].link,
+              originalImgUrl: data[i].link,
+              favorite: false
+            });
+          }
+
+          // +1 page, different from giphy which is an image index offset
+          $scope.offset ++;
+          $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+    } else if ($scope.api === 'Riffsy') {
+      $http.get('http://api.riffsy.com/v1/search?tag=' + $scope.search + '&limit=' + $scope.limit +
+        '&pos=' + $scope.offset)
+        .then(function(response) {
+          var data = response.data.results;
+          $scope.lastData = data;
+
+          console.log(data);
+          for(var i = 0; i < data.length; i++) {
+            $scope.images.push({
+              id: i,
+              imgUrl: data[i].media[0].nanogif.url,
+              hqImgUrl: data[i].media[0].tinygif.url,
+              originalImgUrl: data[i].url,
+              favorite: false
+            });
+          }
+
+          $scope.offset += $scope.limit;
+          $scope.$broadcast('scroll.infiniteScrollComplete');
+      });
+    }
   }
 
   $scope.changeSearch = function() {
@@ -74,4 +120,6 @@ angular.module('starter.controllers', [])
   $scope.$on('$destroy', function() {
     $scope.modal.remove();
   });
-});
+})
+
+;
