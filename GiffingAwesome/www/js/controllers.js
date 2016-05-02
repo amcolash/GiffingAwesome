57dbf15544fbc4d3d86a7364c67cc891a4c99874
Andrew McOlash <amcolash@gmail.com>
2016-05-01 20:39:23 -0500

    Add in trending gifs

--- GiffingAwesome/www/js/controllers.js
+++ GiffingAwesome/www/js/controllers.js
@@ -5,6 +5,7 @@ angular.module('starter.controllers', [])
 
 .controller('SearchController', function($scope, $http, $ionicModal) {
   $scope.api = 'Giphy';
+  $scope.searchtype = 'Search';
   $scope.hq = false;
   $scope.search = 'cats';
 
@@ -15,8 +16,16 @@ angular.module('starter.controllers', [])
 
   $scope.loadImages = function() {
     if ($scope.api === 'Giphy') {
-      $http.get('http://api.giphy.com/v1/gifs/search?q=' + $scope.search + '&limit=' + $scope.limit +
-        '&offset=' + $scope.offset + '&api_key=dc6zaTOxFJmzC')
+      var search;
+      if ($scope.searchtype === 'Search') {
+        search = 'http://api.giphy.com/v1/gifs/search?q=' + $scope.search + '&limit=' + $scope.limit +
+          '&offset=' + $scope.offset + '&api_key=dc6zaTOxFJmzC';
+      } else if ($scope.searchtype === 'Trending') {
+        search = 'http://api.giphy.com/v1/gifs/trending?limit=' + $scope.limit +
+          '&offset=' + $scope.offset + '&api_key=dc6zaTOxFJmzC';
+      }
+
+      $http.get(search)
         .then(function(response) {
           var data = response.data.data;
           $scope.lastData = data;
@@ -36,10 +45,21 @@ angular.module('starter.controllers', [])
           $scope.$broadcast('scroll.infiniteScrollComplete');
         });
     } else if ($scope.api === 'GifMe') {
-      $http.get('http://api.gifme.io/v1/search?query=' + $scope.search + '&limit=' + $scope.limit +
-        '&page=' + $scope.offset + '&key=rX7kbMzkGu7WJwvG')
+      var search;
+      if ($scope.searchtype === 'Search') {
+        search = 'http://api.gifme.io/v1/search?query=' + $scope.search + '&limit=' + $scope.limit +
+          '&page=' + $scope.offset + '&key=rX7kbMzkGu7WJwvG';
+      }
+
+      $http.get(search)
         .then(function(response) {
-          var data = response.data.data;
+          var data;
+          if ($scope.searchtype === 'Search') {
+            data = response.data.data;
+          } else if ($scope.searchtype === 'Trending') {
+            data = response.data.trending;
+          }
+
           $scope.lastData = data;
 
           console.log(data);
@@ -58,8 +78,15 @@ angular.module('starter.controllers', [])
           $scope.$broadcast('scroll.infiniteScrollComplete');
         });
     } else if ($scope.api === 'Riffsy') {
-      $http.get('http://api.riffsy.com/v1/search?tag=' + $scope.search + '&limit=' + $scope.limit +
-        '&pos=' + $scope.offset)
+      var search;
+      if ($scope.searchtype === 'Search') {
+        search = 'http://api.riffsy.com/v1/search?tag=' + $scope.search + '&limit=' + $scope.limit +
+          '&pos=' + $scope.offset;
+      } else if ($scope.searchtype === 'Trending') {
+        search = 'http://api.riffsy.com/v1/trending?limit=50';
+      }
+
+      $http.get(search)
         .then(function(response) {
           var data = response.data.results;
           $scope.lastData = data;
